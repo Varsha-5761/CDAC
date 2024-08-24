0de86e589ef5d3ae084eca9ee3c578bdd23bde50
Varsha-5761 <varshamore5761@gmail.com>
2024-08-24 15:10:44 +0530

    Add files via upload

---
+++ Logic buliding/ASSIGNMENTS/PostiveNumber.java
@@ -0,0 +1,13 @@
+ /*Next Step: Write a Java program that checks if a predefined number is positive using an 
+if-else statement and prints the appropriate message.*/
+class PostiveNumber{
+public static void main(String args[]){
+int number =-6;
+if(number>0){
+System.out.println ("number is positive");
+            }
+else{
+System.out.println ("number is not postive");
+    }   
+ }
+}
